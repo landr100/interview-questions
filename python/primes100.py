cb738194e673735a27ede1ce41c690c3f46c6bfa
Robin <landr100@gmail.com>
2014-10-06 21:25:50 +0000

    update prime

--- python/primes100.py
+++ python/primes100.py
@@ -6,9 +6,8 @@ But for multiples of three print Fizz instead of the number and for the multiple
 For numbers which are multiples of both three and five print FizzBuzz.
 '''
 
-for i in range(100):
-	j = i + 1	
-	if(j%2 == 0) or (j%3 == 0) or (j%5 == 0) or (j%7 == 0):
-		next
+for j in range(2, 100):
+	if ((j%2 == 0) or (j%3 == 0) or (j%5 == 0) or (j%7 == 0)) and j > 7:
+		continue
 	print j
 
