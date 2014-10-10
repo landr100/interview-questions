e2c5feb3a88135517d2d042a5e72b3d97867867f
Robin Gowin <landr100@gmail.com>
2014-10-10 14:01:37 +0000

    update fizzbuzz to include multiples of 3 and 5

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -6,15 +6,13 @@ But for multiples of three print Fizz instead of the number and for the multiple
 For numbers which are multiples of both three and five print FizzBuzz.
 '''
 
-for j in range(100):
-	i = j + 1
-	if(i%3 == 0):
+for i in range(1, 101):
+	if(i%15 == 0):
+		print "FizzBuzz"
+	elif(i%3 == 0):
 		print "Fizz"
 	elif(i%5 == 0):
 		print "Buzz"
 	else:
 		print i
-
-
-
 
